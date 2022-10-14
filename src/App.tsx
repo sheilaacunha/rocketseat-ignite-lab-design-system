4ea5442e1efbe7eea02bb6fd7ca75ccd189b5eb6
Sheila Acunha <johndoe@example.comsheilaacunha1990@gmail.com>
2022-10-14 19:26:30 -0300

    commit

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+
+import './styles/global.css'
+
+function App() {
+
+
+  return (
+    <div className="App">
+
+      <h1 className="fonte-bold text-5xl text-violet-500">ola mundo cretino</h1>
+
+      <button className="bg-cyan-500 font-medium px-4 py-3 rounded-lg hover:bg-cyan-300">
+        enviar
+      </button>
+      
+    </div>
+  )
+}
+
+export default App
